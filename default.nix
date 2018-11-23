d5564287baae2b622c2ea8bf828bedce07b1a8d1
Shaun Sharples <shaun.sharples@gmail.com>
2018-11-23 14:48:20 +0200

    nix 18.09 sha changed?

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 let
   nixpkgs = import (builtins.fetchTarball {
     url = https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz;
-    sha256 = "1lagfycy2lvfc8cdxk98dz2rxjlrbmv9hj42x0x40sy66bck1w0y";
+    sha256 = "1jrzibmq3m74ivwf49nklnrm6prk1ifk4g29jwzdv6yrcfk9bhvv";
   }) {};
 
   inherit (nixpkgs) pkgs;
