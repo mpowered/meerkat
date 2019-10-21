dc8ffa373d3b5ec5a9db3118f04219705a888808
Shaun Sharples <shaun.sharples@gmail.com>
2019-10-22 00:11:50 +0200

    fix nix

--- default.nix
+++ default.nix
@@ -1,15 +1,17 @@
-{ compiler ? "ghc844" }:
+{ compiler ? "ghc864" }:
 
 let
   nixpkgs = import (builtins.fetchTarball {
-    url = https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz;
+    url = https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz;
   }) {};
 
   inherit (nixpkgs) pkgs;
 
   haskellPackages = pkgs.haskell.packages."${compiler}";
 
-  drv = haskellPackages.callCabal2nix "meerkat" ./. {};
+  drv = haskellPackages.callCabal2nix "meerkat" ./. {
+    beam-postgres = with pkgs.haskell.lib; unmarkBroken (dontCheck haskellPackages.beam-postgres);
+  };
 
 in
 {
